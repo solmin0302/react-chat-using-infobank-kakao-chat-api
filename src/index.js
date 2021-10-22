a404cd3b47760fee404be104e96de94d3b153746
서솔민 <solgo123@gmail.com>
2021-10-22 17:53:25 +0900

    chore: prevent double scrolling

--- src/index.js
+++ src/index.js
@@ -1,65 +1,85 @@
-import classNames from 'classnames/bind';
+import classNames from 'classnames/bind'
 import React, { useEffect, useRef, useState } from 'react'
 import styles from './index.css'
-import { Messenger } from './src/Messenger/Messenger';
-import { debounce, rest } from 'lodash';
-import { ChatPopup } from './src/ChatPopup/ChatPopup';
+import { Messenger } from './src/Messenger/Messenger'
+import { debounce, rest } from 'lodash'
+import { ChatPopup } from './src/ChatPopup/ChatPopup'
 
-export const KakaoChat = ({ connectionHeaders, brandId, brandName, serverUrl, userId, ...rest }) => {
-  const [connectedPopup, setConnectedPopup] = useState([]);
-  const [maximumPopupCount, setMaximumPopupCount] = useState(0);
+export const KakaoChat = ({
+  connectionHeaders,
+  brandId,
+  brandName,
+  serverUrl,
+  userId,
+  ...rest
+}) => {
+  const [connectedPopup, setConnectedPopup] = useState([])
+  const [maximumPopupCount, setMaximumPopupCount] = useState(0)
 
-  const debouncedSave = useRef(debounce((width, prevConnectedPopup) => {
-    const remainSpace = width -  316;
-    const expectedMaximumPopupCount = parseInt(remainSpace / 376);
-    setMaximumPopupCount(expectedMaximumPopupCount);
+  const debouncedSave = useRef(
+    debounce((width, prevConnectedPopup) => {
+      const remainSpace = width - 316
+      const expectedMaximumPopupCount = parseInt(remainSpace / 376)
+      setMaximumPopupCount(expectedMaximumPopupCount)
 
-    const newResult = prevConnectedPopup.slice(0,expectedMaximumPopupCount);
-    setConnectedPopup(newResult);
-  }, 500)).current;
+      const newResult = prevConnectedPopup.slice(0, expectedMaximumPopupCount)
+      setConnectedPopup(newResult)
+    }, 500)
+  ).current
 
   const onWindowResize = () => {
-    debouncedSave(window.innerWidth, connectedPopup);
+    debouncedSave(window.innerWidth, connectedPopup)
   }
 
-  useEffect(()=>{
-    const remainSpace = window.innerWidth -  316;
-    const expectedMaximumPopupCount = parseInt(remainSpace / 376);
-    setMaximumPopupCount(expectedMaximumPopupCount);
+  useEffect(() => {
+    const remainSpace = window.innerWidth - 316
+    const expectedMaximumPopupCount = parseInt(remainSpace / 376)
+    setMaximumPopupCount(expectedMaximumPopupCount)
 
-    window.addEventListener('resize', onWindowResize);
-    return ()=>{
-      window.removeEventListener('resize', onWindowResize);
+    window.addEventListener('resize', onWindowResize)
+    return () => {
+      window.removeEventListener('resize', onWindowResize)
     }
-  },[connectedPopup, maximumPopupCount]);
+  }, [connectedPopup, maximumPopupCount])
 
-  const cx = classNames.bind(styles);
-  const onChatPopupRequest = (data)=>{
-    if(connectedPopup.findIndex(d=>d.roomId === data.roomId) !== -1)
-    {
-      return;
+  const cx = classNames.bind(styles)
+  const onChatPopupRequest = (data) => {
+    if (connectedPopup.findIndex((d) => d.roomId === data.roomId) !== -1) {
+      return
     }
 
-    if(connectedPopup.length < maximumPopupCount)
-    {
-      setConnectedPopup([...connectedPopup, data]);
-    }
-    else 
-    {
-      let [first, ...remained] = connectedPopup
-      setConnectedPopup([...remained, data]);
+    if (connectedPopup.length < maximumPopupCount) {
+      setConnectedPopup([...connectedPopup, data])
+    } else {
+      const [first, ...remained] = connectedPopup
+      setConnectedPopup([...remained, data])
     }
   }
 
   const closePopup = (data) => {
-    const newResult = connectedPopup.filter((d)=>d.roomId !== data.roomId);
-    setConnectedPopup(newResult);
+    const newResult = connectedPopup.filter((d) => d.roomId !== data.roomId)
+    setConnectedPopup(newResult)
   }
 
   return (
     <div className={cx('kakao-using-infobank-main')}>
-      <Messenger onChatPopupRequest={onChatPopupRequest} connectionHeaders={connectionHeaders} brandId={brandId} serverUrl={serverUrl} brandName={brandName}/>
-      {connectedPopup.map((data)=><ChatPopup onClose={closePopup} data={data} key={data.roomId} connectionHeaders={connectionHeaders} brandId={brandId} serverUrl={serverUrl} />)}
+      <Messenger
+        onChatPopupRequest={onChatPopupRequest}
+        connectionHeaders={connectionHeaders}
+        brandId={brandId}
+        serverUrl={serverUrl}
+        brandName={brandName}
+      />
+      {connectedPopup.map((data) => (
+        <ChatPopup
+          onClose={closePopup}
+          data={data}
+          key={data.roomId}
+          connectionHeaders={connectionHeaders}
+          brandId={brandId}
+          serverUrl={serverUrl}
+        />
+      ))}
     </div>
   )
 }
