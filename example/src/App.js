a0be7136bc141cefba34ec3352461b04a3a94006
서솔민 <solgo123@gmail.com>
2021-10-25 15:36:58 +0900

    chore: remove secret / remove duplicate props

--- example/src/App.js
+++ example/src/App.js
@@ -1,14 +1,13 @@
 import React from 'react';
-
 import { KakaoChat } from 'react-chat-using-infobank-kakao-chat-api';
 import 'react-chat-using-infobank-kakao-chat-api/dist/index.css';
 
 const App = () => {
   const token = localStorage.getItem('influencer-token');
+  const { REACT_APP_CHAT_SERVER_URL } = process.env;
   const connectHeaders = {
     Authorization: `Bearer ${token}`,
   };
-  const { REACT_APP_CHAT_SERVER_URL } = process.env;
 
   return (
     <div className="container">
@@ -17,7 +16,7 @@ const App = () => {
         connectionHeaders={connectHeaders}
         brandId="1"
         serverUrl={REACT_APP_CHAT_SERVER_URL}
-        brandName={'민호네'}
+        brandName="민호네"
       />
     </div>
   );
