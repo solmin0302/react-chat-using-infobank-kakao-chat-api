edfc55e82d7149e0c7e38fdda2afef51204f0f71
서솔민 <solgo123@gmail.com>
2021-10-25 15:13:15 +0900

    chore: remove secret / remove duplicate props

--- example/src/App.js
+++ example/src/App.js
@@ -1,26 +1,26 @@
-import React from 'react'
+import React from 'react';
 
-import { KakaoChat } from 'react-chat-using-infobank-kakao-chat-api'
-import 'react-chat-using-infobank-kakao-chat-api/dist/index.css'
+import { KakaoChat } from 'react-chat-using-infobank-kakao-chat-api';
+import 'react-chat-using-infobank-kakao-chat-api/dist/index.css';
 
 const App = () => {
-
-  const SERVER_URL = 'https://influencer-chat.fnf.co.kr';
+  const token = localStorage.getItem('influencer-token');
   const connectHeaders = {
-    "Authorization": "Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiI1WGZYa0ZIZEdJeThLd25hVENLSHJ0d3MiLCJpYXQiOjE2MzQ1Mjc1MzIsImV4cCI6MTYzOTcxMTUzMn0.Ye0-InDPIo8nFNdrENOLxmh0pJWFi1Qw7yorDm_mOCo",
+    Authorization: `Bearer ${token}`,
   };
+  const { REACT_APP_CHAT_SERVER_URL } = process.env;
 
   return (
     <div className="container">
-      <div className='bg'></div>
+      <div className="bg"></div>
       <KakaoChat
         connectionHeaders={connectHeaders}
-        brandId={"1"}
-        serverUrl={SERVER_URL}
-        brandName={"민호네"}
+        brandId="1"
+        serverUrl={REACT_APP_CHAT_SERVER_URL}
+        brandName={'민호네'}
       />
     </div>
-  ) 
-}
+  );
+};
 
-export default App
+export default App;
